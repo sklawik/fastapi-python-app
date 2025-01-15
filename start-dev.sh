67b2a0a21dd3287f9a45ee9070a009d9d5f1a4d9
sklawik <sklawik@MacBook-Air-Sebastian.local>
2025-01-15 15:53:52 +0100

    start dev script for auto change listening

---
+++ start-dev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+python3 -c "import fastapi uvicorn"
+if [ $? -eq 0 ]; then
+    echo 'Znaleziono FastAPI'
+else
+    echo 'Nie znaleziono FastAPI lub uvicorn. Instalacja:'
+    pipx install fastapi
+    pipx install uvicorn
+fi
+
+echo 'uruchamianie skryptu..'
+uvicorn main:app --reload
