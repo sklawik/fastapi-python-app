1caabc766efbae4afa454c38c45441210e430275
sklawik <sklawik@MacBook-Air-Sebastian.local>
2025-01-16 00:44:16 +0100

    branch test

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-htmlFile=""
 
-with open("./main.html", 'r') as file:
-    htmlFile = file.read()
 
+
+
+
+
+
+@app.get('/bst')
+async def root():
+    
+    numbers = {5, 4, 3, 2, 1}
+    return {"data": [1,2,3,4,5]}
+
+
+# strona html serwowana domyślnie
 @app.get('/')
 async def root():
+    with open("./main.html", 'r') as file:
+        htmlFile = file.read()
     return HTMLResponse(content=htmlFile)
+
+items = []
+
+@app.get("/items")
+def create_item(item: str):
+    items.append(item)
+    return items.__len__()
+
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int)->str:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail="not found (item)")
