6648fffc8c64aad0e2b91a5c2f6cf3b395aa39c7
sklawik <sklawik@MacBook-Air-Sebastian.local>
2025-01-16 16:25:26 +0100

    linked list basics

--- main.py
+++ main.py
@@ -4,19 +4,41 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 
 class Node:
-    value: int
-    parent: Node
-    children: Node
-    def __init__(value):
-        
-
-class LinkedList:
-    rootNode: Node
-    def __init__(self):
-        print("class init")
+    def __init__(self, value):
+        self.value: int = value
+        self.parent: Node = None
+        self.children: Node = None
     
+    
+class LinkedList:
+    def __init__(self):
+        self.rootNode: Node = None 
+        
+    def print_linked_list(self):
+        current = self.rootNode
+        if current is None:
+            print("Linked list is empty")
+        else:
+            while current is not None:
+                print(current.value)
+                current = current.parent 
+
+    def insert(self, value):
+        if self.rootNode is None:
+            self.rootNode = Node(value) 
+        else:
+            current = self.rootNode
+            while current.parent is not None:
+                current = current.parent  
+            current.parent = Node(value) 
 
 
+linkedList = LinkedList()
+linkedList.insert(5)
+linkedList.insert(15)
+linkedList.insert(0)
+
+linkedList.print_linked_list()
         
 
 htmlFile=""
